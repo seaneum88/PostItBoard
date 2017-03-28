907385c31692f9f7bd223472208721de58554863
tkdrb31 <seaneum88@gmail.com>
2017-03-28 10:25:35 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>PostitBoard</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" />
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css" />
 
     <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
-    <script src="script.js"></script>
+    <script src="js/script.js"></script>
     <?php
     /**
      */
@@ -19,13 +20,11 @@
 <nav class="navbar navbar-default">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="/">Angular Single App Example</a>
+            <a class="navbar-brand" href="#">Angular Single App Example</a>
         </div>
 
         <ul class="nav navbar-nav navbar-right">
             <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
-            <!-- <li><a href="#about"><i class="fa fa-shield"></i> About</a></li>
-            <li><a href="#contact"><i class="fa fa-comment"></i> Contact</a></li> -->
             <li><a href="#registration"><i class="fa fa-shield"></i> Registration</a></li>
             <li><a href="#login"><i class="fa fa-comment"></i> Log in</a></li>
         </ul>
@@ -37,12 +36,14 @@
     <!-- angular templating -->
     <!-- this is where content will be injected -->
     <div ng-view></div>
-
 </div>
 
 <footer class="text-center">
-      <p>Copyright to John & Sean</p>
+    <p>Copyright to John & Sean</p>
 </footer>
 
+<script src="//code.jquery.com/jquery-1.11.3.min.js" type="text/javascript"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+<script src="js/note.js"></script>
 </body>
 </html>
